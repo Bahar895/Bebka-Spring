eb5b4c2644f293663d903877452e41925b2d22fd
Bahar Aydın <bhrs_01@hotmail.com>
2025-09-15 18:43:07 +0300

    SP-10-Security Config

--- src/main/java/com/bahar/Bebka_Spring/BebkaSpringApplication.java
+++ src/main/java/com/bahar/Bebka_Spring/BebkaSpringApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BebkaSpringApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BebkaSpringApplication.class, args);
     }
